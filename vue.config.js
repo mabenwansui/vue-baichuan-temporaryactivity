f650b30122ac5bce4ec64b188dbc7b89ca937af7
maben <maben@liepin.com>
2020-10-12 11:39:17 +0800

    解决多html页注入问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+const fs = require('fs')
+const path = require('path')
 const HtmlCustomPlugin = require('./htmlCustomPlugin')
+const HtmlWebpackPlugin = require('@vue/cli-service/node_modules/html-webpack-plugin')
+
+const getHtmls = src => fs.readdirSync(src).filter(v=> /\.html?$/.test(v)).filter(v=> v!=='index.html')
 
 module.exports = {
   publicPath: '',
@@ -9,27 +14,21 @@ module.exports = {
     https: true
   },
   crossorigin: 'anonymous',
-  configureWebpack: {
-    plugins: [new HtmlCustomPlugin()]
-  },
-  chainWebpack: config => {    
-    config
-      .plugin('html')
-      .tap((args) => {
-        let [options] = args
-        options = {
-          ...options,
-          ...{
-            minify: false,
-            inject: true,
-          }
-        }
-        return args
+  configureWebpack(config) {
+    let htmlOptions
+    config.plugins.forEach(v=> {      
+      if (v instanceof HtmlWebpackPlugin) {
+        htmlOptions = {...v.options}
+      }
+    })
+    if(htmlOptions) {
+      let files = getHtmls('./public')
+      files.forEach(v=> {
+        htmlOptions.template = path.join(__dirname, 'public', v)
+        htmlOptions.filename = v
+        config.plugins.push(new HtmlWebpackPlugin(htmlOptions))
       })
+    }
+    config.plugins.push(new HtmlCustomPlugin())
   }
 }
-
-
-
-
-
